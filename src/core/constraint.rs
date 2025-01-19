facaab846f9ef0b15ec70b9c1aaee686dbf8a0c8
Mohsen Pakzad <mohsen.pakzad99@gmail.com>
2025-01-19 13:28:25 +0330

    change: imbed RefCell in ConstrRef for inner mutability

--- src/core/constraint.rs
+++ src/core/constraint.rs
@@ -1,4 +1,4 @@
-use std::{fmt, rc::Rc};
+use std::{cell::RefCell, fmt, rc::Rc};
 
 use crate::core::expression::LinearExpr;
 
@@ -20,47 +20,49 @@ pub enum ConstraintSense {
 }
 
 #[derive(Debug, Clone)]
-pub struct ConstrRef(Rc<Constraint>);
+pub struct ConstrRef(Rc<RefCell<Constraint>>);
 
 impl ConstrRef {
     pub fn new(lhs: LinearExpr<VarRef>, sense: ConstraintSense, rhs: LinearExpr<VarRef>) -> Self {
-        Self(Rc::new(Constraint {
+        Self(Rc::new(RefCell::new(Constraint {
             name: None,
             lhs,
             sense,
             rhs,
-        }))
+        })))
     }
 
-    pub fn name(mut self, name: impl Into<String>) -> Self {
-        self.constraint().name = Some(name.into());
+    pub fn name(self, name: impl Into<String>) -> Self {
+        self.0.borrow_mut().name = Some(name.into());
         self
     }
 
     pub fn get_sense(&self) -> ConstraintSense {
-        self.0.sense
+        self.0.borrow().sense.clone()
     }
 
-    pub fn get_lhs(&self) -> &LinearExpr<VarRef> {
-        &self.0.lhs
+    pub fn get_lhs(&self) -> LinearExpr<VarRef> {
+        self.0.borrow().lhs.clone()
     }
 
-    pub fn get_rhs(&self) -> &LinearExpr<VarRef> {
-        &self.0.rhs
-    }
-
-    fn constraint(&mut self) -> &mut Constraint {
-        Rc::make_mut(&mut self.0)
+    pub fn get_rhs(&self) -> LinearExpr<VarRef> {
+        self.0.borrow().rhs.clone()
     }
 }
 
 impl fmt::Display for ConstrRef {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let sense = match &self.0.sense {
+        let sense = match &self.0.borrow().sense {
             ConstraintSense::LessEqual => "<=",
             ConstraintSense::GreaterEqual => ">=",
             ConstraintSense::Equal => "=",
         };
-        write!(f, "{} {} {}", &self.0.lhs, sense, &self.0.rhs)
+        write!(
+            f,
+            "{} {} {}",
+            &self.0.borrow().lhs,
+            sense,
+            &self.0.borrow().rhs
+        )
     }
 }
